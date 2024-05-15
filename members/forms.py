6d87f4da2e92c757f4f4c9a1002ff3fd7c5dd581
Turki-55 <143620160+Turki-55@users.noreply.github.com>
2024-05-15 17:49:05 +0300

    Add files via upload

---
+++ members/forms.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.contrib.auth.models import User 
+from django import forms
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', "first_name", "last_name","email","password1","password2")
+    
+    def __init__(self, *args, **kwags):
+        super(SignUpForm,self).__init__(*args, **kwags)
+
+        self.fields['username'].widget.attrs['class'] = "form-control"
+        self.fields['password1'].widget.attrs['class'] = "form-control"
+        self.fields['password2'].widget.attrs['class'] = "form-control"
+
+
+
+class EditProfileForm(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', "first_name", "last_name","email")
